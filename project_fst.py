37e0e0925cba1fe1035d289869a4915da2af907d
Joel Lee <32894264+jleex@users.noreply.github.com>
2017-12-01 14:02:33 -0800

    Add files via upload

--- project_fst.py
+++ project_fst.py
@@ -1,42 +1,41 @@
+import mrcfile
 import numpy as np
-import mrcfile as mf
-import scipy as sp
+import matplotlib.pyplot as plt
+from scipy.interpolate import RegularGridInterpolator as RGI
+from numpy.fft import fftn, fftshift, ifft2
 
-zika_file = mf.open('zika_153.mrc')  #give the full path to the file here
+zika_file = mrcfile.open('zika_153.mrc', mode='r+')
+mol = zika_file.data
 
+def project_fst(rho, R):
+    N = rho.shape[0]
+    N_range = np.linspace(-1, 1, N)
+    rho_hat_f = RGI((N_range, N_range, N_range), rho, bounds_error=False, method='linear', fill_value=0)
+   
+    grid = np.concatenate((N_range.reshape(N, 1, 1, 1) * np.ones(N * N).reshape(1, N, N, 1),
+                          N_range.reshape(1, N, 1, 1) * np.ones(N * N).reshape(N, 1, N, 1),
+                          N_range.reshape(1, 1, N, 1) * np.ones(N * N).reshape(N, N, 1, 1)),
+                         axis=3)
+    
+    
+    rho_hat = rho_hat_f(np.dot(grid, R))
+    
+    return np.real(ifft2(fftshift(fftn(rho_hat))[:,:,0]))
 
-def project_fast(rho, a, b):
-  rho_hat = np.fft.fftn(np.fft.fftshift(rho))
-  
-  N = length(rho)
-  
-  for i in range(N):
-    for j in range(N):
-      eta_x[i,j] = -N/2 + 1 + j*1
-      eta_y[i,j] = 1 - N/2 + i
-  
-  eta_x, eta_y = [...,np.newaxis]  #check this
-  
-  grid = eta_x*a + eta_y*b        #sampling grid
-  
-  
-  
-  #sample rho_hat
-  from sp import RegularGridInterpolator as RGI
-  rho_hat_f = sp.interpolate.RGI(rho_hat, CHECK THIS )
-  
-  imageab = rho_hat_f(grid)
-  
-  finalimage = np.fft.ifftn(np.fft.ifftshift(imageab))
-  realfinalimage = np.real(finalimage)
-  
-  from matplotlib import pyplot as plt
+def random_rotation_matrix():
+    e_1 = 2*np.random.rand(3) - 1
+    e_1 /= np.linalg.norm(e_1)
 
-  plt.imshow(realfinalimage)
+    e_2 = np.cross( e_1, 2*np.random.rand(3) - 1 )
+    e_2 /= np.linalg.norm(e_2)
 
+    e_3 = np.cross( e_1, e_2 )
+    e_3 /= np.linalg.norm(e_3)
 
-try:
-  rho = zika_file.data
-  print(rho)
-  a = [1,0,0]
-  b = [0,1,0]
+    return np.array([e_1, e_2, e_3]).T
+
+project_fst(mol, random_rotation_matrix())
+
+image = project_fst(mol, random_rotation_matrix())
+plt.imshow(image)
+plt.show()
